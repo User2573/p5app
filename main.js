ce28b8448edbf9c502a77c9f8af641e25fe48ef7
User2573 <lol>
2023-10-13 14:07:46 -0400

    schoolproject become index; change to three js

--- main.js
+++ main.js
@@ -1,23 +1,2 @@
-let myShader;
-
-function preload() {
-    myShader = loadShader('glsl/shader.vert', 'glsl/shader.frag');
-}
-
-function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-
-
-
-
-function draw() {
-    shader(myShader);
-    myShader.setUniform('iResolution', [width, height])
-    rect(0, 0, width, height);
-}
+import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.min.js';
+const canvas = document.getElementById('canvas');
