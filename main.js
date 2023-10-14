cd108633ffb01d295706f66a67f2f85091e52206
User2573 <71093251+User2573@users.noreply.github.com>
2023-10-13 20:49:39 -0400

    comitting before pull

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-let myShader;
-
 function preload() {
-    myShader = loadShader('glsl/shader.vert', 'glsl/shader.frag');
+    console.log('preload :)');
 }
 
 function setup() {
@@ -12,12 +10,58 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-
-
-
-
+const rects = [];
 function draw() {
-    shader(myShader);
-    myShader.setUniform('iResolution', [width, height])
-    rect(0, 0, width, height);
+    background(200);
+    for (let i in rects) {
+        const r = rects[i];
+        strokeWeight(0);
+        fill(r.c);
+        const x = Math.min(r.x0, r.x1);
+        const X = Math.max(r.x0, r.x1);
+        const y = Math.min(r.y0, r.y1);
+        const Y = Math.max(r.y0, r.y1);
+        rect(x - windowWidth/2, y - windowHeight/2, X - x, Y - y);
+
+
+        if (!r.editing) {
+            const rand = () => -10+20*Math.random();
+            rects[i] = {
+                c: r.c,
+                editing: r.editing,
+                x0: r.x0 + rand(),
+                x1: r.x1 + rand(),
+                y0: r.y0 + rand(),
+                y1: r.y1 + rand(),
+            };
+        }
+    }
+}
+
+
+
+
+function mousePressed() {
+    const rand = () => 255 * Math.random();
+    rects.push({
+        x0: mouseX,
+        y0: mouseY,
+        x1: mouseX,
+        y1: mouseY,
+        c: color(rand(), rand(), rand()),
+        editing: true
+    });
+}
+
+function mouseDragged() {
+    if (rects.length > 0) {
+        const r = rects[rects.length-1];
+        r.x1 = mouseX;
+        r.y1 = mouseY;
+        rects[rects.length-1] = r;
+    }
+}
+
+function mouseReleased() {
+    rects[rects.length-1].editing = false;
 }
--- main0.js
+++ main0.js
@@ -1,5 +1,7 @@
+let myShader;
+
 function preload() {
-    console.log('preload :)');
+    myShader = loadShader('glsl/shader.vert', 'glsl/shader.frag');
 }
 
 function setup() {
@@ -10,49 +12,17 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-const rects = [];
+
+
+
+
 function draw() {
     background(200);
-    for (const r of rects) {
-        strokeWeight(0);
-        fill(r.c);
-        const x = Math.min(r.x0, r.x1);
-        const X = Math.max(r.x0, r.x1);
-        const y = Math.min(r.y0, r.y1);
-        const Y = Math.max(r.y0, r.y1);
-        rect(x - windowWidth/2, y - windowHeight/2, X - x, Y - y);
 
 
-        if (!r.editing) {
-            
-        }
-    }
-}
-
-
-
-
-function mousePressed() {
-    const rand = () => 255 * Math.random();
-    rects.push({
-        x0: mouseX,
-        y0: mouseY,
-        x1: mouseX,
-        y1: mouseY,
-        c: color(rand(), rand(), rand()),
-        editing: true
-    });
-}
-
-function mouseDragged() {
-    if (rects.length > 0) {
-        const r = rects[rects.length-1];
-        r.x1 = mouseX;
-        r.y1 = mouseY;
-        rects[rects.length-1] = r;
-    }
-}
-
-function mouseReleased() {
-    rects[rects.length-1].editing = false;
+    rotateY(0.5);
+    box(30, 50);
+    // shader(myShader);
+    // myShader.setUniform('iResolution', [width, height])
+    // rect(0, 0, width, height);
 }
