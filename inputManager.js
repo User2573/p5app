0a9e6587258946fada9ce28704df05569f7c2e98
User2573 <71093251+User2573@users.noreply.github.com>
2023-10-13 22:02:17 -0400

    working show password; error message styled

--- inputManager.js
+++ inputManager.js
@@ -2,8 +2,21 @@ const loginButton = document.getElementById('login');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 
-loginButton.onclick = e => {
+const toggleShowPassword = document.getElementById('showpw');
+let show = false;
+const updateShowPassword = s =>  {
+    show = s;
+    toggleShowPassword.style.opacity = show ? '1' : '.6';
+    toggleShowPassword.textContent = show ? '\ue8f4' : '\ue8f5';
+    passwordInput.type = show ? 'text' : 'password';
+};
+toggleShowPassword.addEventListener('click', () => passwordInput.focus());
+toggleShowPassword.addEventListener('click', () => updateShowPassword(!show));
+passwordInput.addEventListener('blur', () => updateShowPassword(false));
+
+
+loginButton.addEventListener('click', e => {
     e.preventDefault();
     console.log(usernameInput.value);
     console.log(passwordInput.value);
-}
+})
