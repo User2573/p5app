4f892c9c5191a292c71e2bcfc977ba43cc8fcd82
User2573 <71093251+User2573@users.noreply.github.com>
2023-10-29 14:11:36 -0400

    broken border alpha blending shader

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -11,6 +11,7 @@ const mousePosition = new THREE.Vector2();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+scene.background = new THREE.Color('rgb(7, 7, 10)');
 
 
 
@@ -32,16 +33,22 @@ import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 
 
 const composer = new EffectComposer(renderer);
+const computedStyle = getComputedStyle(document.getElementsByTagName('canvas')[0]);
+console.log(computedStyle.getPropertyValue('--border-color'));
 const highlightShader = new ShaderPass({
 	uniforms: {
 		'tDiffuse': { value: null },
-        'uCorner' : { value: new THREE.Vector2(0) },
-        'uDimensions' : { value: new THREE.Vector2(0) },
-        'uMouse': { value: new THREE.Vector2(0) }, 
+        'uCorner' : { value: new THREE.Vector2() },
+        'uDimensions' : { value: new THREE.Vector2() },
+        'uBorderRadius': { value: parseFloat(computedStyle.borderRadius) },
+        'uBorderWidth': { value: parseFloat(computedStyle.getPropertyValue('--border-width')) },
+        'uBorderColor': { value: new THREE.Vector4(...computedStyle.getPropertyValue('--border-color').match(/[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)/g).map((n, i) => parseFloat(n) / (i == 3 ? 1 : 255))) },
+        'uMouse': { value: new THREE.Vector2() }, 
 	},
 	vertexShader: `varying vec2 UV;void main(){UV=uv;gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1);}`,
 	fragmentShader: await (await fetch('scripts/app/mouseHover.frag')).text()
 });
+console.log(highlightShader.material.uniforms)
 composer.addPass(new RenderPass(scene, camera));
 composer.addPass(highlightShader);
 composer.addPass(new OutputPass());
@@ -52,6 +59,8 @@ window.addEventListener('mousemove', e => {
     mousePosition.y = e.clientY;
     highlightShader.material.uniforms.uMouse.value = mousePosition;
 });
+
+window.dispatchEvent(new Event('mousemove'));
 
 window.addEventListener('resize', () => {
     const { x, y, width, height } = canvas.parentNode.getBoundingClientRect();
--- scripts/app/mouseHover.frag
+++ scripts/app/mouseHover.frag
@@ -2,6 +2,9 @@ uniform sampler2D tDiffuse;
 uniform vec2 uCorner;
 uniform vec2 uDimensions;
 uniform vec2 uMouse;
+uniform float uBorderRadius;
+uniform float uBorderWidth;
+uniform vec4 uBorderColor;
 varying vec2 UV;
 
 
@@ -10,19 +13,24 @@ void main() {
     vec3 texel = texture2D(tDiffuse, UV).xyz;
 
     vec2 texelPos = uCorner + (vec2(UV.x, 1.-UV.y)) * uDimensions;
-
-    float dist2 = dot(texelPos - uMouse, texelPos - uMouse);
-    float brightness = sin(exp(-.0005*dist2));
     
-    float r = 50.;
-    vec2 q = abs(texelPos-uCorner-uDimensions/2.)-uDimensions/2.+r;
-    float borderDist = r - min(max(q.x,q.y),0.0) - length(max(q,0.0));
 
-    texel += vec3(.005 * brightness);
+
+    
+    vec2 q = abs(texelPos-uCorner-uDimensions/2.)-uDimensions/2.+uBorderRadius;
+    float borderDist = uBorderRadius - min(max(q.x,q.y),0.0) - length(max(q,0.0));
 
     float falloff = .5;
-    float inBorder = smoothstep(4.+falloff, 4.-falloff, borderDist);
-    texel = mix(texel, pow(texel,vec3(.5))+.05*vec3(21,20,36)/255., inBorder);
+    float inBorder = smoothstep(uBorderWidth+falloff, uBorderWidth-falloff, borderDist);
+    
+    float alpha = 1.;
+    texel = mix(texel, inBorder*uBorderColor.xyz, uBorderColor.w);
+
+
+    float dist2 = dot(texelPos - uMouse, texelPos - uMouse);
+    float mouseHighlight = .005 * sin(exp(-dist2/pow(75., 2.)));
+    mouseHighlight = mix(mouseHighlight, pow(mouseHighlight, .7), inBorder);
+    //texel += vec3(mouseHighlight);
 
 
     gl_FragColor = vec4(texel, 1);
